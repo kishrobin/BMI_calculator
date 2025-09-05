7508889ded4c38ae87671cf0bf14ef393aeccead
kishrobin <robinsonkishore@gmail.com>
2025-09-05 19:46:55 +0530

    update codes

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-/*let movie;
-movie1 ="master";
-movie2 ="beast"
-console.log(movie1);*/
-
-
-// let person1 = 50;
-// let person2 = 55;
-// let person3 =33;
-// let totalamt=(person1+person2+person3);
-// console.log(totalamt);
-
-
-
-
-// let leoblockbuster =true;
-// console.log(leoblockbuster);
-
-
-
-// let gay="arulmozhi";
-// let pombalasoku ="kavi";
-// let gaji ="robin";
-// let lavadagang =(gaji+gay+pombalasoku);
-// console.log(lavadagang);
-
-// let kavikaranpombala_porukii=true;
-// console.log(kavikaranpombala_porukii);
-
-
-// let athimbel ="gaji No:1";
-// let bob ="local item no1";
-// let declareorginalty=(bob + athimbel);
-// console.log(declareorginalty);
-
 function calculatevalue() {
 
     let weightInput = prompt("Enter your weight in Kg :");
@@ -46,5 +11,19 @@ function calculatevalue() {
     let bmi=weight/(height*height);
 
     document.getElementById("result").innerHTML = `your BMI value is: ${bmi .toFixed(2)}`;
+    let status =`you are ${bmi}`;
+
+    if(bmi<=18.4){
+        status="you are underweight";
+    }else if(bmi>=18.5 && bmi<=24.9){
+        status="you are healthy";
+    }else if(bmi>=25.0 && bmi<=29.9){
+        status="you are overweight";
+    }else{
+        status="you are obese..";
+    }
+    document.getElementById("status").innerHTML=status;
+
+
     
 }
